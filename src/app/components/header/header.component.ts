87671d7cfd94d834329e7f11d9ab1e438388f9bf
Shrey Raj <shreyraj1653@gmail.com>
2025-01-30 22:43:14 +0530

    add:nav

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,10 +10,23 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
   imports: [CommonModule, RouterModule, NzIconModule],
   templateUrl: './header.component.html'
 })
+
 export class AppHeaderComponent {
   navigationItems = [
-    { label: 'Add Workout', route: '/add-workout', icon: 'plus-circle' },
-    { label: 'Workout List', route: '/workout-list', icon: 'unordered-list' },
-    { label: 'Progress', route: '/workout-chart', icon: 'bar-chart' }
+    { label: 'Add Workout', route: '#add-workout', icon: 'plus-circle' },
+    { label: 'Workout List', route: '#workout-list', icon: 'unordered-list' },
+    { label: 'Progress', route: '#workout-chart', icon: 'bar-chart' }
   ];
+
+  // Function to handle smooth scrolling
+  scrollToSection(event: Event, route: string) {
+    event.preventDefault();  // Prevent default anchor link behavior
+    const targetElement = document.querySelector(route);
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.getBoundingClientRect().top + window.scrollY,
+        behavior: 'smooth'
+      });
+    }
+  }
 }
